f753ad6364f41bf1a0dfbaea08be1decf820e900
LaurenCurtis <lauren_curtis1@baylor.edu>
2021-12-09 11:29:55 -0600

    Migrating model

--- Pizzeria/settings.py
+++ Pizzeria/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #My Apps
+    'pizzas',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
